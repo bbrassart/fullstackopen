b6bbc93629910241d5f2dec9c82b0b2e8e05f69b
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-10-30 17:28:25 +0100

    Exercise 1.2. courseinfo step 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = props => {
-  return (
-    <h1>{props.course}</h1>
-  )
-};
+const Header = props => <h1>{props.course}</h1>;
+const Part = props => <p>{props.part} {props.exercises}</p>;
+const Footer = props => <p>Number of exercises {props.total}</p>;
 
 const Content = props => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
-  )
-};
-
-const Footer = props => {
-  return (
-    <p>Number of exercises {props.total}</p>
   )
 };
 
