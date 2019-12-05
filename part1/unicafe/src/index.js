5daa586ee551f9fe084b91196a6398b7df0c3a8d
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-12-05 16:17:29 +0100

    Exercise 1.8. unicafe step 3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,24 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = ({clicks}) => {
+  const { good, neutral, bad, total } = clicks;
+  const getAverage = () => total ? (good - bad) / total : 0;
+  const getPositivePercentage = () => total ? (good / total) * 100 : 0;
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {getAverage()}</p>
+      <p>positive {getPositivePercentage()} %</p>
+    </div>
+  );
+}
+
 const App = () => {
   const [clicks, setClicks] = useState({
     good: 0,
@@ -19,23 +37,13 @@ const App = () => {
     setClicks(newClicks);
   };
 
-  const getAverage = () => clicks.total ? (clicks.good - clicks.bad) / clicks.total : 0;
-
-  const getPositivePercentage = () => clicks.total ? (clicks.good / clicks.total) * 100 : 0;
-
   return (
     <div>
       <h2>give feedback</h2>
       <button onClick={() => increase('good')}>good</button>
       <button onClick={() => increase('neutral')}>neutral</button>
       <button onClick={() => increase('bad')}>bad</button>
-      <h2>statistics</h2>
-      <p>good {clicks.good}</p>
-      <p>neutral {clicks.neutral}</p>
-      <p>bad {clicks.bad}</p>
-      <p>all {clicks.total}</p>
-      <p>average {getAverage()}</p>
-      <p>positive {getPositivePercentage()} %</p>
+      <Statistics clicks={clicks} />
   </div>
   );
 };
