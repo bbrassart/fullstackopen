82fccbd897b0e73e0552431528af570e6d661450
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-12-05 12:31:03 +0100

    Exercise 1.5. courseinfo step 5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,46 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = props => <h1>{props.course}</h1>;
-const Part = props => <p>{props.part} {props.exercises}</p>;
-const Footer = props => <p>Number of exercises {props.total}</p>;
+const Part = props => <p>{props.part.name} {props.part.exercises}</p>;
 
-const Content = props => {
+const Total = props => {
+  const total = props.parts.map(part => part.exercises).reduce((a, b) => a + b, 0);
   return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </>
+    <p>Number of exercises {total}</p>
   )
 };
 
+const Content = props => props.parts.map(part => <Part key={part.name} part={part} />);
+
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const total = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1.name}
-        part2={part2.name}
-        part3={part3.name}
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises} />
-      <Footer total={total} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 };
