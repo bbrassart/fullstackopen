df9b60ead1c3d83bd9a6fe3a612fd7113e6c6201
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-12-17 18:56:55 +0100

    Exercise 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]);
   const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
-  const isDuplicate = userInput => persons.some(person => person.name === userInput);
-  const renderNames = persons => persons.map(person => <li key={person.name}>{person.name}</li>);
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
 
-  const addName = (event) => {
+  const isDuplicate = userInput => persons.some(person => person.name === userInput);
+
+  const renderContacts = persons => {
+    return persons.map(person => <li key={person.name}>{person.name} {person.number}</li>);
+  };
+
+  const addContact = (event) => {
     event.preventDefault();
 
     if (isDuplicate(newName)) {
@@ -21,24 +29,34 @@ const App = () => {
       return;
     }
 
-    const newPersonObject = { name: newName };
+    const newPersonObject = { name: newName, number: newNumber };
     setPersons(persons.concat(newPersonObject));
     setNewName('');
+    setNewNumber('');
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <input
-          value={newName}
-          onChange={handleNameChange}
-        />
-        <button type="submit">add</button>
+      <form onSubmit={addContact}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+       <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
       <ul>
-        { renderNames(persons) }
+        { renderContacts(persons) }
       </ul>
     </div>
   )
