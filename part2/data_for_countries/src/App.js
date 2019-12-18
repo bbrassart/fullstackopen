aecdbeb89a25d384421b267210a870ea26f61db4
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-12-18 10:22:59 +0100

    Exercise 2.12* Data for countries, step1

---
+++ part2/data_for_countries/src/App.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const FullCountry = ({country}) => {
+  const flagStyles = {
+    width: "150px"
+  };
+
+  return (
+    <div>
+      <h3>{country.name}</h3>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {
+          country.languages.map(lang => <li key={lang.iso639_1}>{lang.name}</li>)
+        }
+      </ul>
+      <img alt='country flag' src={country.flag} style={flagStyles} />
+    </div>
+  );
+};
+
+const MinimalCountry = ({country}) => {
+  return (
+    <li>
+      {country.name}
+    </li>
+  )
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  const handleSearchTermChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const hook = () => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data);
+      })
+  };
+
+  useEffect(hook, []);
+
+  const filteredCountries =
+    countries.filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  const renderCountries = countries => {
+    if (countries.length === 1) {
+      return <FullCountry country={countries[0]} />;
+    }
+    return (
+      <ul>{ countries.map(country => <MinimalCountry key={country.name} country={country} />) }</ul>
+    );
+  };
+
+  return (
+    <div>
+      <input value={searchTerm} onChange={handleSearchTermChange} />
+      { filteredCountries.length > 10 && <p>Too many matches, specify another filter</p> }
+      { filteredCountries.length < 10 && renderCountries(filteredCountries) }
+    </div>
+  );
+};
+
+export default App;
