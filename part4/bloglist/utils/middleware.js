f7016646ab66f414cd3fdb527d53e1352360b6dc
bbrassart <brassart@gmail.com>
2020-02-28 15:52:31 +0100

    Exercise 4.13 Blog list expansions, step1

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,6 +1,8 @@
 const validationErrorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
   next(error);
