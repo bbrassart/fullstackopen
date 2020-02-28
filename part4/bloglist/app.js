4d4b4876b888e16ed661697a44f97232d5ff0cb6
bbrassart <brassart@gmail.com>
2020-02-28 18:27:57 +0100

    Exercise 4.15: bloglist expansion, step4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 
@@ -17,7 +19,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   });
 
 app.use(bodyParser.json());
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 app.use(middleware.validationErrorHandler);
 
 module.exports = app;
